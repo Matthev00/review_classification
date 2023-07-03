3dc3d964d405abaa3a9300e504039b7066ea4ef4
Mateusz Ostaszewski <mateuszostaszewskififa@gmail.com>
2023-07-03 17:55:06 +0200

    created ReviewContainer class works as a dataloader

--- data.py
+++ data.py
@@ -1,3 +1,8 @@
+from random import shuffle
+from typing import List
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
 class Category:
     ELECTRONICS = "ELECTRONICS"
     BOOKS = "BOOKS"
@@ -26,3 +31,40 @@ class Review:
             return Sentiment.NEUTRAL
         else:
             return Sentiment.POSITIVE
+
+
+class ReviewContainer:
+    def __init__(self, reviews: List[Review]):
+        self.reviews = reviews
+
+    def evenly_distibute(self):
+        negatives = []
+        neutrals = []
+        positives = []
+        for review in self.reviews:
+            if review.sentiment == Sentiment.NEGATIVE:
+                negatives.append(review)
+            elif review.sentiment == Sentiment.NEUTRAL:
+                neutrals.append(review)
+            else:
+                positives.append(review)
+
+        len_of_samples = min(len(negatives), len(neutrals), len(positives))
+        positives = positives[:len_of_samples]
+        neutrals = neutrals[:len_of_samples]
+        negatives = negatives[:len_of_samples]
+
+        self.reviews = positives + neutrals + negatives
+        shuffle(self.reviews)
+
+    def get_text(self):
+        return [x.text for x in self.reviews]
+
+    def get_sentiment(self):
+        return [x.sentiment for x in self.reviews]
+
+    def get_X(self, vectorizer: TfidfVectorizer):
+        return vectorizer.transform(self.get_text())
+
+    def get_category(self):
+        return [x.category for x in self.reviews]
